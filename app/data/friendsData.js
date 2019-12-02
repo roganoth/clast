55e2ae738d4a9cc831707b979d837deb4cda5acb
dan demeo <chronos937@gmail.com>
2019-12-01 22:18:55 -0500

    added additional sample friends to the friendsData.js

--- app/data/friendsData.js
+++ app/data/friendsData.js
@@ -3,6 +3,21 @@ var friends = [
         name: "Spongebob Squarepants",
         photoLink: "https://images2.minutemediacdn.com/image/upload/c_fill,g_auto,h_1248,w_2220/f_auto,q_auto,w_1100/v1555921064/shape/mentalfloss/spongebob_0_0.jpg",
         scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    },
+    {
+        name: "Macho Man Randy Savage",
+        photoLink: "https://img.bleacherreport.net/img/images/photos/002/318/574/macho-man-randy-savage-1_crop_north.jpg?h=533&w=800&q=70&crop_x=center&crop_y=top",
+        scores: [1,2,3,3,4,5,3,3,1,1]
+    },
+    {
+        name: "Olaf",
+        photoLink: "https://media.comicbook.com/2017/12/olafs-frozen-adventure-1062970-1280x0.jpg",
+        scores: [3,3,3,3,3,3,3,3,3,3]
+    },
+    {
+        name: "Obi Wan Kenobi",
+        photoLink: "https://lumiere-a.akamaihd.net/v1/images/Obi-Wan-Kenobi_6d775533.jpeg?region=0%2C0%2C1536%2C864&width=960",
+        scores: [1,1,1,1,1,1,1,1,1,5]
     }
 ];
 
